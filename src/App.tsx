545223768a3a3c5b5c5a4decf55dd3d8d76d3c4c
Tmbaldwi <Tyler.Baldwin.Work@gmail.com>
2024-09-01 19:29:41 -0700

    removed test api

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ const App: React.FC = () => {
 
   return (
     <div style={styles.screen}>
-      <FullStackTestComponent/>
       {!imageSrc &&
       <div style={styles.importImageContainer}>
         <h1>Select an Image :)</h1>
